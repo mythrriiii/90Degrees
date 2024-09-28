a47606a1ad590040c52618df1f931378e56f486b
leozheng829 <leo.zheng.2022@gmail.com>
2024-09-27 21:49:45 -0400

    intial frontend layout from tutorial

---
+++ Authentication-API/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/users");
+const errorHandler = require("./middlewares/errorHandler");
+const app = express();
+//! Connect to mongodb
+mongoose
+  .connect("mongodb+srv://andyvvo2004:<db_password>@hackgtcluster.0ui9y.mongodb.net/?retryWrites=true&w=majority&appName=HackGTCluster")
+  .then(() => console.log("Db connected successfully"))
+  .catch((e) => console.log(e));
+
+//! Middlewares
+app.use(express.json()); //pass incoming json data from the user
+//!Routes
+app.use("/", router);
+//!error handler
+app.use(errorHandler);
+//! Start the server
+const PORT = 8000;
+app.listen(PORT, console.log(`Server is up and running`));
