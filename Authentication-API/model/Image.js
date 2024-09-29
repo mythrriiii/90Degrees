2629b2ea8efb1de395c3a26f5375f38a09ece573
Andy Vo <andy.v.vo2004@gmail.com>
2024-09-29 02:13:33 -0400

    tested api

--- Authentication-API/model/Image.js
+++ Authentication-API/model/Image.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const imageSchema = new mongoose.Schema({
-  url: String,
+    url: String,
+    email: {type: String, required: true},
 });
 
 const Image = mongoose.model('Image', imageSchema);
